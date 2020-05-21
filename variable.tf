4ba207405218e88a011104d35ba645a0af789595
denissboboljov <65718788+denissboboljov@users.noreply.github.com>
2020-05-21 17:24:16 +0300

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "credentials" {
+    default = "bodeniss.json"
+}
+variable "project" {
+  default = " triple-rookery-277909"
+  description = "Project id"
+}
+
+variable "region" {
+  default = "europe-north1"
+  description = "region"
+}
+
+variable "zone" {
+  default = "europe-north1-a"
+  description = "zone"
+}
